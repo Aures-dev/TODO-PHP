e3253c952c298b24aad6321456ec61c1ebf1da41
Aures-dev <auresaz69@gmail.com>
2024-11-18 18:24:30 +0100

    Router.php créé

--- public/index.php
+++ public/index.php
@@ -1,7 +1,29 @@
 <?php 
-
 //Autoloader
 // spl_autoload_register(function ($class) {
 //     require "../src/$class.php";
 // });
-require "../src/Views/layout.php";
+
+//------------------------------------Début
+use App\Router;
+
+
+//Autoloader
+require dirname(__DIR__)."/vendor/autoload.php";
+
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+
+// Créer une instance du routeur
+$router = new Router();
+
+$router->get("/", function () {});
+$router->post("/add", function () {});
+$router->get("/add", function () {});
+$router->get("/toggle", function () {});
+$router->get("/delete", function () {});
+
+echo "<pre>";
+var_dump($router);
