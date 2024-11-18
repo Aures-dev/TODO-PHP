18ad0d035e7fb9c5e8c7e59a102923aa5187eaef
Aures-dev <auresaz69@gmail.com>
2024-11-18 18:23:31 +0100

    TodoController créé

---
+++ src/Controllers/TodoController.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Controllers;
+
+class TodoController{
+
+    public function index(){
+        //Récupérer les tâches depuis la session
+        if (!isset($_SESSION)) {
+            session_start();//récupérer la session existante
+        }
+
+        $todos = $_SESSION["todos"];
+
+        //Charger le vue "Views/index.php"
+            // require __DIR__ ."/../Views/index.php";
+        require dirname(__DIR__) ."/Views/index.php";
+    }
+    public function add(){
+    }
+
+    public function delete(){
+    }
+
+    public function toggle(){
+    }
+}
